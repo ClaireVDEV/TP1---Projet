43af5405aa1c48d10cecc1d5a2eac79b81830f4d
clair <clair@172.17.149.109>
2024-11-07 16:30:03 +0100

    push

--- TP1/src/TP1/TP1CLASSTEST.java
+++
@@ -1,9 +0,0 @@
-package TP1;
-
-public class TP1CLASSTEST {
-	
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
-
-}
---
+++ TP1/src/personnages/Humain.java
@@ -0,0 +1,22 @@
+package personnages;
+
+public class Humain {
+	
+	//arguments
+	private String nom;
+	private String BoissonPref;
+	private float ArgentPossede;
+	
+	//methodes
+	private void nom(type nomvar) {
+		nom = nomvar;
+	}
+	
+	//constructeur
+	private Humain() {
+		this.ArgentPossede;
+		this.nom;
+		this.BoissonPref;
+	}
+
+}
